41ff52c26cec5cbd83182fc3d783985cb8bfa10a
Yuan Zhou <yz882@cornell.edu>
2019-12-03 16:40:04 -0500

    Update 3d rendering. Remove prints.

--- 3d-rendering/utils.cpp
+++ 3d-rendering/utils.cpp
@@ -52,7 +52,7 @@ int read_input(std::string path_to_data, bit32** input)
   {
     int num_3d_tri;
     infile >> num_3d_tri;
-    printf("%d\n", num_3d_tri);
+    printf("%d triangles\n", num_3d_tri);
     *input = (bit32*)calloc(3 * MAX_NUM_TRIS, sizeof(bit32));
 
     // pack input data for better performance
@@ -60,7 +60,7 @@ int read_input(std::string path_to_data, bit32** input)
     {
       unsigned int x0, y0, z0, x1, y1, z1, x2, y2, z2;
       infile >> x0 >> y0 >> z0 >> x1 >> y1 >> z1 >> x2 >> y2 >> z2;
-      printf("%d %d %d %d %d %d %d %d\n", x0, y0, z0, x1, y1, z1, x2, y2, z2);
+      //printf("%d %d %d %d %d %d %d %d\n", x0, y0, z0, x1, y1, z1, x2, y2, z2);
       
       (*input)[3*i](7,0)     = x0;
       (*input)[3*i](15,8)    = y0;
